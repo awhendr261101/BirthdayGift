6509b297209bd664f781526a80a8c021f4996719
awhendr261101 <awodwasihlehendrick@gmail.com>
2025-02-07 12:54:08 +0200

    error output

--- test_obstacles.py
+++ test_obstacles.py
@@ -15,3 +15,14 @@ def captured_io(stdin):
         yield sys.stdout, sys.stderr
     finally:
         sys.stdout, sys.stderr, sys.stdin = old_out, old_err, old_input
+
+@contextmanager
+def captured_output():
+    """Capture standard error and output and make that available for testing"""
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
